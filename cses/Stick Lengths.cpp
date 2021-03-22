94f72b4935d6b9a926ac654f270501348ed7d13f
ssk-2610 <shubhamsubhash.k18@iiits.in>
2021-03-22 22:11:17 +0530

    new

---
+++ cses/Stick Lengths.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define lli long long int
+#define li long int
+#define ld long double
+using namespace std;
+const lli mod = 1e9 + 7;
+
+lli compute_cost(vector<int> arr, int target)
+{
+	lli cost = 0;
+	for (auto ele : arr)
+	{
+		cost += abs(target - ele);
+	}
+	return cost;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> arr(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	sort(arr.begin(), arr.end());
+	int median = arr[n / 2];
+	cout << compute_cost(arr, median);
+	return 0;
+}
