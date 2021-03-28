9e15753c83fa177401c11f6907cb7ec2e4cbad98
ssk-2610 <shubhamsubhash.k18@iiits.in>
2021-03-28 11:36:39 +0530

    .

---
+++ cses/Subarray Sums I.py
@@ -0,0 +1,17 @@
+n,x=map(int,input().split())
+a=list(map(int,input().split()))
+d=dict()
+d[0]=1
+ct=0
+sum1=0
+b=[0]
+for i in range(n):
+    sum1+=a[i]
+    if sum1 in d:
+        d[sum1]+=1
+    else:
+        d[sum1]=1
+    q=sum1-x
+    if q in d:
+        ct+=d[q]
+print(ct)
