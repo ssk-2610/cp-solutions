173673660f06fcd3325cadc5b1448ccf7407dcfc
ssk-2610 <shubhamsubhash.k18@iiits.in>
2021-04-12 09:23:50 +0530

    .

---
+++ codeforces/Kefa and First Steps.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define ld long double
+using namespace std;
+const ll mod = 1e9 + 7;
+#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define inp(a,n) for(int i=0;i<n;i++){cin>>a[i];}
+#define out(a,n) for(int i=0;i<n;i++){cout<<a[i]<<" ";}
+
+int main()
+{
+    IOS;
+    int a,c=0,d=0;
+    cin>>a;
+    int b[a];
+    for(int i=0;i<a;i++){
+    cin>>b[i];
+    if(b[i]>=b[i-1])c++;
+    else c=1;
+    d=max(d,c);
+    }
+    cout <<d;
+    
+}
